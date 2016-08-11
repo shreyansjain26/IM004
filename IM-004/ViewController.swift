658f830c8e14e63e8e966889dc00cb242717b607
Shreyans Jain <shreyans.j@practo.com>
2016-08-11 15:32:05 +0530

    Initial Commit

---
+++ IM-004/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  IM-004
+//
+//  Created by Shreyans Jain on 11/08/16.
+//  Copyright © 2016 sj. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
