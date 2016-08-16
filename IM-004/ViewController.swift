82e9c017a134695f24cbcda023a6d82f18b687df
Shreyans Jain <shreyans.j@practo.com>
2016-08-16 11:47:01 +0530

    notification

--- IM-004/ViewController.swift
+++ IM-004/ViewController.swift
@@ -9,10 +9,23 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    
+    @IBOutlet weak var inText: UITextField!
+    @IBOutlet weak var notificationButton: UIButton!
+    
+    private var foregroundNotification: NSObjectProtocol!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        if(!UIApplicationLaunchOptionsLocalNotificationKey.isEmpty ) {
+            let ac = UIAlertController(title: "Alert", message:inText.text, preferredStyle: .Alert)
+            ac.addAction(UIAlertAction(title: "OK", style: .Default, handler: nil))
+            presentViewController(ac, animated: true, completion: nil)
+            
+        }
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +33,19 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func getNotification(sender: AnyObject) {
+    
+        let notificationSettings = UIUserNotificationSettings(forTypes: [.Alert, .Badge, .Sound], categories: nil)
+        UIApplication.sharedApplication().registerUserNotificationSettings(notificationSettings)
+        let notification = UILocalNotification()
+        notification.fireDate = NSDate(timeIntervalSinceNow: 6)
+        notification.alertBody = inText.text
+        notification.alertAction = "Notification"
+        notification.soundName = UILocalNotificationDefaultSoundName
+        notification.userInfo = ["CustomField1": "w00t"]
+        UIApplication.sharedApplication().scheduleLocalNotification(notification)
+    
+    }
 
 }
 
